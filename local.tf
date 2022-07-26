8e21c06608f72fd63487137cb3df88cf800331ab
Thalyson Rodrigues <tha.motog@gmail.com>
2022-07-26 10:00:14 -0400

    add: data resource

--- local.tf
+++ local.tf
@@ -8,10 +8,18 @@ variable "content" {
   default = "Hello world!"
 }
 
+data "local_file" "content-exemplo" {
+  filename = "${path.module}/hello.txt"
+}
+
 output "id-do-arquivo" {
   value = resource.local_file.helloworld.id
 }
 
 output "content" {
   value = var.content
+}
+
+output "data-resource-result" {
+  value = data.local_file.content-exemplo.content
 }
